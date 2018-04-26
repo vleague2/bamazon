515b2d16b9605271c38da9deb19edf58e16e76fa
unknown <vleague2@gmail.com>
2018-04-26 18:40:15 -0400

    working on user action function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
+// set up required modules
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const table = require('console.table');
 
+// set up items connection to database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,43 +12,97 @@ const connection = mysql.createConnection({
     database: "bamazon_db"
 })
 
+// connect to database and start the program
 connection.connect((err) => {
     if (err) throw err;
     console.log("Connected as " + connection.threadId);
+
+    // call the function to start the program
     readDB();
+
 })
 
-
+// read the database's current entries
 function readDB() {
     connection.query(
         "SELECT * FROM products", (err, res) => {
             if (err) throw err;
-            console.log(res);
-            console.log(res[0].id + res[0].product_name + res[0].department_name + res[0].price + res[0].stock_quantity);
- 
-            const tableHead = [
-                'Item #',
-                'Product',
-                'Department',
-                'Price',
-                'Quantity'
-            ]
-
+    
+            // set up empty array that will hold each value from the database response
             let tableVal = [];
 
+            // loop through database response and push each value to the array
             for (i=0; i < res.length; i++) {
-                let array = [
-                    res[i].id,
-                    res[i].product_name,
-                    res[i].department_name,
-                    res[i].price,
-                    res[i].stock_quantity
-                ];
+                let array = {
+                    item: res[i].id,
+                    product: res[i].product_name,
+                    department: res[i].department_name,
+                    price: res[i].price,
+                    quantity: res[i].stock_quantity
+                };
                 
                 tableVal.push(array);
             }
 
-            console.table(tableHead, tableVal);
+            // display a table containing the array items
+            console.table(tableVal);
+
+            // call the next function to ask the user what they want to purchase, and pass in tableVal so we can use it there
+            userAction(tableVal);
         }
     )
+}
+
+// function to ask the user what action they'd like to take
+function userAction(tableVal) {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the ID # of the item you would like to purchase?",
+            name: "item",
+
+            // make sure the item ID is one of the available options
+            validate: function(input) {
+                if (input > tableVal.length) {
+                    return false;
+                }
+
+                else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "How many would you like to purchase?",
+            name: "quantity"
+        }
+    ])
+    .then(answers => {
+
+        // set up a variable that we will adjust to contain the item the user has chosen
+        let item = "";
+
+        // loop through tableVal array and check if the user's choice matches that item's ID. if so, reassign the variable above
+        tableVal.forEach(val => {
+            if (answers.item == val.item) {
+                item = val;
+            }
+        });
+
+        // if the quantity the user has specified is more than the quantity available
+        if (answers.quantity > item.quantity) {
+
+            // log that the quantity is insufficient, and re-call the function to restart the questions
+            console.log("Insufficient quantity!\n\n");
+            userAction(tableVal);
+        }
+
+        // console.log (item);
+
+        // if (answers.quantity) {
+            
+        // }
+        // console.log(answers);
+    })
 }

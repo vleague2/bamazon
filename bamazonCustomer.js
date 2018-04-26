4b9574a14f42bbe69f50577803cfa82004df6368
unknown <vleague2@gmail.com>
2018-04-26 17:36:15 -0400

    created readDB function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const table = require('console.table');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -21,6 +22,31 @@ function readDB() {
         "SELECT * FROM products", (err, res) => {
             if (err) throw err;
             console.log(res);
+            console.log(res[0].id + res[0].product_name + res[0].department_name + res[0].price + res[0].stock_quantity);
+ 
+            const tableHead = [
+                'Item #',
+                'Product',
+                'Department',
+                'Price',
+                'Quantity'
+            ]
+
+            let tableVal = [];
+
+            for (i=0; i < res.length; i++) {
+                let array = [
+                    res[i].id,
+                    res[i].product_name,
+                    res[i].department_name,
+                    res[i].price,
+                    res[i].stock_quantity
+                ];
+                
+                tableVal.push(array);
+            }
+
+            console.table(tableHead, tableVal);
         }
     )
 }

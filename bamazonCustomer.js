596c4bbd3455901700e4f7193ec2e254deaaa50a
unknown <vleague2@gmail.com>
2018-04-26 19:58:53 -0400

    added a constructor and updated naming

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,16 @@ const inquirer = require('inquirer');
 const table = require('console.table');
 const chalk = require('chalk');
 
+
+let Product = function(id, name, dept, price, quant) {
+    this.Item = id;
+    this.Product = name;
+    this.Department = dept;
+    this.Price = price;
+    this.Quantity = quant;
+}
+
+
 // set up items connection to database
 const connection = mysql.createConnection({
     host: "localhost",
@@ -30,33 +40,27 @@ function readDB() {
             if (err) throw err;
     
             // set up empty array that will hold each value from the database response
-            let tableVal = [];
+            let productsArray = [];
 
             // loop through database response and push each value to the array
             for (i=0; i < res.length; i++) {
-                let array = {
-                    item: res[i].id,
-                    product: res[i].product_name,
-                    department: res[i].department_name,
-                    price: res[i].price,
-                    quantity: res[i].stock_quantity
-                };
+                let itemObject = new Product(res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity);
                 
-                tableVal.push(array);
+                productsArray.push(itemObject);
             }
 
             // display a table containing the array items
             console.log("\n")
-            console.table(tableVal);
+            console.table(productsArray);
 
-            // call the next function to ask the user what they want to purchase, and pass in tableVal so we can use it there
-            userAction(tableVal);
+            // call the next function to ask the user what they want to purchase, and pass in productsArray so we can use it there
+            userAction(productsArray);
         }
     )
 }
 
 // function to ask the user what action they'd like to take
-function userAction(tableVal) {
+function userAction(productsArray) {
     inquirer.prompt([
         {
             type: "input",
@@ -65,7 +69,7 @@ function userAction(tableVal) {
 
             // make sure the item ID is one of the available options
             validate: function(input) {
-                if (input > tableVal.length) {
+                if (input > productsArray.length) {
                     return false;
                 }
 
@@ -83,34 +87,34 @@ function userAction(tableVal) {
     .then(answers => {
 
         // set up a variable that we will adjust to contain the item the user has chosen
-        let item = "";
+        let userItem = "";
 
-        // loop through tableVal array and check if the user's choice matches that item's ID. if so, reassign the variable above
-        tableVal.forEach(val => {
-            if (answers.item == val.item) {
-                item = val;
+        // loop through productsArray array and check if the user's choice matches that item's ID. if so, reassign the variable above
+        productsArray.forEach(product => {
+            if (answers.item == product.Item) {
+                userItem = product;
             }
         });
 
         // if the quantity the user has specified is more than the quantity available
-        if (answers.quantity > item.quantity) {
+        if (answers.quantity > userItem.Quantity) {
 
             // log that the quantity is insufficient, and re-call the function to restart the questions
             console.log(chalk.red("\nInsufficient quantity!\n"));
-            userAction(tableVal);
+            userAction(productsArray);
         }
 
         else {
-            console.log(chalk.bgGreen("\n\nYou have purchased " + answers.quantity + " " + item.product + "s!\nThe total cost was: $" + answers.quantity * item.price));
-            updateDB(answers.quantity, item);
+            console.log(chalk.bgGreen("\n\n   You have purchased " + answers.quantity + " " + userItem.Product + "s!   \n   The total cost was: $" + answers.quantity * userItem.Price + "   "));
+            updateDB(answers.quantity, userItem);
         }
     })
 }
 
 
-function updateDB(quantity, item) {
+function updateDB(quantity, userItem) {
 
-    let newQuant = item.quantity - quantity;
+    let newQuant = userItem.Quantity - quantity;
 
     connection.query(
         "UPDATE products SET ? WHERE ?",
@@ -119,7 +123,7 @@ function updateDB(quantity, item) {
                 stock_quantity: newQuant
             },
             {
-                id: item.item
+                id: userItem.Item
             }
         ],
         function(err, res) {

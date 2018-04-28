c1981519aff53185a870f27312212a2d2d34d52f
unknown <vleague2@gmail.com>
2018-04-28 14:27:25 -0400

    three out of four functions operating in manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,63 +18,86 @@ connection.connect((err) => {
     if (err) throw err;
     console.log("\nWelcome to Bamazon!\n");
     
+    // start the program
+    start();
+})
+
+function start() {
+
+    // query database for an updated list of all products
     connection.query(
         "SELECT * FROM products", (err, res) => {
             if (err) throw err;
-            // call the function to start the program
-            start(res);
+
+            // prompt the user to select what they want to do
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "What would you like to do?",
+                    choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+                    name: "action"
+                }
+            ])
+            .then(answers => {
+
+                let action = answers.action;
+
+                // based on what they choose, select a function and pass in the response from the database if needed
+                switch (action) {
+                    case "View Products for Sale": 
+                        viewProducts(res);
+                        break;
+                    case "View Low Inventory":
+                        lowInventory();
+                        break;
+                    case "Add to Inventory":
+                        addInventory(res);
+                        break;
+                    case "Add New Product":
+                        newProduct();
+                        break;
+                }    
+            })
         }
     )
-})
-
-function start(res) {
-
-    
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
-            name: "action"
-        }
-    ])
-    .then(answers => {
-        let action = answers.action;
-        switch (action) {
-            case "View Products for Sale": 
-                viewProducts(res);
-                break;
-            case "View Low Inventory":
-                lowInventory(res);
-                break;
-            case "Add to Inventory":
-                addInventory(res);
-                break;
-            case "Add New Product":
-                newProduct();
-                break;
-        }    
-    })
 }
 
+// function to display all products
 function viewProducts(res) {
+
+    // display the results from the database
     console.table(res);
-    start(res);
+
+    // recall the start function
+    start();
 }
 
+// function to display items with low inventory
+function lowInventory() {
 
-function lowInventory(res) {
+    // query the database
     connection.query(
-        "SELECT * FROM products WHERE stock_quantity < 5", (err, res2) => {
+
+        // for items that have less than 5 quantity
+        "SELECT * FROM products WHERE stock_quantity < 5", (err, res) => {
             if (err) throw err;
-            console.table(res2);
-            start(res);
+
+            // display the result in a table
+            console.table(res);
+
+            // recall the start function
+            start();
         }
     )
 }
 
+// function to add to the inventory
 function addInventory(res) {
+
+    // display the database in a table so the user can see the items
     console.table(res);
+
+    // prompt them for the item they want to restock and how much
     inquirer.prompt([
         {
             type: "input",
@@ -106,16 +129,17 @@ function addInventory(res) {
     ])
     .then(answers => {
 
+        // take the item number they chose and subtract one so that we can use it as an array index number
         let itemIndex = answers.item - 1;
 
-        // set up a variable that we will adjust to contain the item the user has chosen
+        // assign the array entry to a variable so we can access it
         let userItem = res[itemIndex];
 
+        // take the amount they entered and assign it to a variable
         let amount = parseInt(answers.amount);
 
+        // calculate the new quantity after the restock 
         let newQuant = userItem.stock_quantity + amount;
-
-        console.log(newQuant);
 
         // query the database to update
         connection.query(
@@ -133,17 +157,11 @@ function addInventory(res) {
 
             function(err, res3) {
                 if (err) throw err;
+
+                // let the user know how much they've restocked
                 console.log(chalk.bgGreen("\n\n   You have added " + amount + " to " + userItem.product_name + "     \n"));
 
-                // send another query to database to get updated response and pass into start function
-                connection.query(
-                    "SELECT * FROM products", (err, res) => {
-                        if (err) throw err;
-                        // call the function to start the program
-                        start(res);
-                    }
-                )
-                
+                start();
             }
         )
     })
